08a816e1182f3153252f009cf559506da392bf0b
marco-rubio-bustos <marco.rubio.bustos@gmail.com>
2025-07-14 01:19:23 -0400

    Se actualizan las rutas para incluir nuevos endpoint.

--- controllers/reservasController.js
+++ controllers/reservasController.js
@@ -12,10 +12,34 @@ export const obtenerReservas = async (req, res) => {
   }
 };
 
+export const obtenerReservasIndividual = async (req, res) => {
+  try {
+    const { reservaIndividual } = req.params;
+    const data = await fs.readFile(getReservas, 'utf-8');
+    const reserva = JSON.parse(data);
+    const resultado = reserva.findIndex(
+      (r) => r.reservas === Number(reservaIndividual)
+    );
+
+    if (resultado === -1) {
+      return res
+        .status(404)
+        .json({ mensaje: 'Reserva no encontrada con ese número' });
+    }
+
+    res.json({
+      mensaje: 'Reserva encontrada',
+      reserva: reserva[resultado],
+    });
+  } catch (error) {
+    res.status(500).json({ mensaje: 'Error al obtener las reservas' });
+  }
+};
+
 export const crearReserva = async (req, res) => {
   try {
     const data = await fs.readFile(getReservas, 'utf-8');
-    const reservas = JSON.parse(data);
+    const reserva = JSON.parse(data);
     const {
       hotel,
       reservas: nombreReserva,
@@ -29,7 +53,7 @@ export const crearReserva = async (req, res) => {
     } = req.body;
 
     const ultimoId =
-      reservas.length > 0 ? Math.max(...reservas.map((r) => r.id)) : 0;
+      reserva.length > 0 ? Math.max(...reserva.map((r) => r.id)) : 0;
 
     const nueva = {
       id: ultimoId + 1,
@@ -54,30 +78,26 @@ export const crearReserva = async (req, res) => {
 
 export const actualizarReserva = async (req, res) => {
   try {
-    const id = parseInt(req.params.id);
+    const { id } = req.params;
     const data = await fs.readFile(getReservas, 'utf-8');
-    const reservas = JSON.parse(data);
+    const reserva = JSON.parse(data);
+    const resultado = reserva.findIndex((r) => r.reservas === Number(id));
 
-    console.log(id, ' - ', typeof id);
-
-    const index = reservas.findIndex((r) => r.reservas === id);
-    console.log(index);
-
-    if (index === -1) {
+    if (resultado === -1) {
       return res
         .status(404)
         .json({ mensaje: 'Reserva no encontrada con ese número' });
     }
 
-    reservas[index] = {
-      ...reservas[index],
+    reserva[index] = {
+      ...reserva[index],
       ...req.body,
     };
-    await fs.writeFile(getReservas, JSON.stringify(reservas, null, 2));
+    await fs.writeFile(getReservas, JSON.stringify(reserva, null, 2));
 
     res.json({
       mensaje: 'Reserva actualizada con éxito',
-      reserva: reservas[index],
+      reserva: reserva[index],
     });
   } catch (error) {
     console.error(error);
@@ -85,51 +105,158 @@ export const actualizarReserva = async (req, res) => {
   }
 };
 
-/////////////////////////////////////////
+export const eliminarReserva = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const data = await fs.readFile(getReservas, 'utf-8');
+    const reserva = JSON.parse(data);
+    const resultado = reserva.findIndex((r) => r.id === Number(id));
 
-export const eliminarReserva = (req, res) => {
-  const id = parseInt(req.params.id);
-  const index = reservas.findIndex((r) => r.id === id);
-  if (index !== -1) {
-    const eliminada = reservas.splice(index, 1);
-    res.json(eliminada[0]);
-  } else {
-    res.status(404).json({ mensaje: 'La reserva no fue encontrada' });
+    if (resultado !== -1) {
+      const eliminada = reserva.splice(index, 1);
+      await fs.writeFile(getReservas, JSON.stringify(reserva, null, 2));
+
+      res.json({
+        mensaje: 'La reserva fue eliminada con éxito',
+        reserva: eliminada[0],
+      });
+    } else {
+      res.status(404).json({ mensaje: 'La reserva no fue encontrada' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ mensaje: 'Error al eliminar la reserva' });
   }
 };
 
 // filtros
-export const filtrarPorHotel = (req, res) => {
-  const { nombre } = req.params;
+export const filtrarPorHotel = async (req, res) => {
+  try {
+    const { hotel } = req.params;
+    const data = await fs.readFile(getReservas, 'utf-8');
+    const reserva = JSON.parse(data);
+    const resultado = reserva.filter(
+      (r) => r.hotel.toLowerCase() === hotel.toLowerCase()
+    );
 
-  res.json(
-    reservas.filter((r) => r.hotel.toLowerCase() === nombre.toLowerCase())
-  );
+    if (resultado.length === 0) {
+      return res
+        .status(404)
+        .json({ mensaje: 'No se encontraron reservas para ese hotel' });
+    }
+
+    res.json({
+      mensaje: 'Reserva encontradas',
+      reserva: resultado,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ mensaje: 'Error al filtrar la reserva' });
+  }
 };
 
-export const filtrarPorFecha = (req, res) => {
-  const { fecha } = req.params;
-  res.json(reservas.filter((r) => r.fecha === fecha));
+export const filtrarPorFecha = async (req, res) => {
+  try {
+    const { fecha_inicio, fecha_fin } = req.query;
+    const data = await fs.readFile(getReservas, 'utf-8');
+    const reserva = JSON.parse(data);
+    const resultado = reserva.filter((r) => {
+      return (
+        r.fecha_inicio &&
+        r.fecha_fin &&
+        r.fecha_inicio >= fecha_inicio &&
+        r.fecha_fin <= fecha_fin
+      );
+    });
+
+    if (resultado.length === 0) {
+      return res.status(404).json({
+        mensaje: 'No se encontraron reservas entre las fechas indicadas',
+      });
+    }
+
+    res.json({
+      mensaje: 'Reservas encontradas en el rango de fechas',
+      reservas: resultado,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ mensaje: 'Error al filtrar la reserva por fechas' });
+  }
 };
 
-export const filtrarPorCliente = (req, res) => {
-  const { nombre } = req.params;
-  res.json(
-    reservas.filter((r) => r.cliente.toLowerCase() === nombre.toLowerCase())
-  );
+export const filtrarPorHabitacion = async (req, res) => {
+  try {
+    const { habitacion } = req.params;
+    const data = await fs.readFile(getReservas, 'utf-8');
+    const reserva = JSON.parse(data);
+    const resultado = reserva.filter(
+      (r) => r.tipo_habitacion.toLowerCase() === habitacion.toLowerCase()
+    );
+
+    if (resultado.length === 0) {
+      return res.status(404).json({
+        mensaje: 'No se encontraron habitaciones indicadas',
+      });
+    }
+
+    res.json({
+      mensaje: 'Reservas encontradas según tipo de habitación',
+      reservas: resultado,
+    });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ mensaje: 'Error al filtrar la reserva por habitación' });
+  }
 };
 
-export const ordenarPorFecha = (req, res) => {
-  const ordenadas = [...reservas].sort(
-    (a, b) => new Date(a.fecha) - new Date(b.fecha)
-  );
-  res.json(ordenadas);
+export const filtrarPorEstado = async (req, res) => {
+  try {
+    const { estado } = req.params;
+    const data = await fs.readFile(getReservas, 'utf-8');
+    const reserva = JSON.parse(data);
+    const resultado = reserva.filter(
+      (r) => r.estado.toLowerCase() === estado.toLowerCase()
+    );
+
+    if (resultado.length === 0) {
+      return res.status(404).json({
+        mensaje: 'No se encontraron el estado indicado',
+      });
+    }
+
+    res.json({
+      mensaje: 'Reservas encontradas según tipo de estado',
+      reservas: resultado,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ mensaje: 'Error al filtrar la reserva por estado' });
+  }
 };
 
-export const obtenerActivas = (req, res) => {
-  res.json(reservas.filter((r) => r.estado === 'activa'));
-};
+export const filtrarPorHuesped = async (req, res) => {
+  try {
+    const data = await fs.readFile(getReservas, 'utf-8');
+    const reserva = JSON.parse(data);
+    const resultado = reserva.filter((r) => r.num_huespedes > 5);
 
-export const obtenerCanceladas = (req, res) => {
-  res.json(reservas.filter((r) => r.estado === 'cancelada'));
+    if (resultado.length === 0) {
+      return res.status(404).json({
+        mensaje: 'No existen más de 5 huéspedes',
+      });
+    }
+
+    res.json({
+      mensaje: 'Reservas encontradas con más de 5 huéspedes',
+      reservas: resultado,
+    });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ mensaje: 'Error al filtrar la reserva por huéspedes' });
+  }
 };
--- routes/reservasRoutes.js
+++ routes/reservasRoutes.js
@@ -1,29 +1,31 @@
-import {Router} from "express";
+import { Router } from 'express';
 import {
   obtenerReservas,
+  obtenerReservasIndividual,
   crearReserva,
   actualizarReserva,
   eliminarReserva,
-  filtrarPorCliente,
-  filtrarPorFecha,
   filtrarPorHotel,
-  ordenarPorFecha,
-  obtenerActivas,
-  obtenerCanceladas,
-} from "../controllers/reservasController.js";
+  filtrarPorFecha,
+  filtrarPorHabitacion,
+  filtrarPorEstado,
+  filtrarPorHuesped,
+  // obtenerCanceladas,
+} from '../controllers/reservasController.js';
 
 const router = Router();
 
-router.get("/", obtenerReservas);
-router.post("/", crearReserva);
-router.put("/:id", actualizarReserva);
-router.delete("/:id", eliminarReserva);
+router.get('/', obtenerReservas);
+router.get('/reservaIndividual/:reservaIndividual', obtenerReservasIndividual);
+router.post('/', crearReserva);
+router.put('/:id', actualizarReserva);
+router.delete('/:id', eliminarReserva);
 
-router.get("/cliente/:nombre", filtrarPorCliente);
-router.get("/fecha/:fecha", filtrarPorFecha);
-router.get("/hotel/:nombre", filtrarPorHotel);
-router.get("/ordenar/fecha", ordenarPorFecha);
-router.get("/activas", obtenerActivas);
-router.get("/canceladas", obtenerCanceladas);
+router.get('/filtrar/:hotel', filtrarPorHotel);
+router.get('/fecha', filtrarPorFecha);
+router.get('/habitacion/:habitacion', filtrarPorHabitacion);
+router.get('/estado/:estado', filtrarPorEstado);
+router.get('/huesped', filtrarPorHuesped);
+// router.get("/canceladas", obtenerCanceladas);
 
 export default router;
